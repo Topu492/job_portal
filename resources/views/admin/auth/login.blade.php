26a62ec83fe828761bdcc290f0d7f2fae83c8e4f
Topu492 <topu86702@gmail.com>
2026-01-21 11:37:54 +0600

    Admin Panel Mastering - Forget Page

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -6,7 +6,7 @@
       <div class="row">
         <div class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
           <div class="login-brand">
-            <img src="{{ config('settings.site_logo') }}" alt="logo" width="200" class="shadow-light">
+            <img src="assets/img/stisla-fill.svg" alt="logo" width="100" class="shadow-light rounded-circle">
           </div>
 
           <div class="card card-primary">
@@ -59,7 +59,7 @@
           </div>
 
           <div class="simple-footer">
-            Copyright &copy; websolutionus {{ date('Y') }}
+            Copyright &copy; Md Saiful Islam {{ date('Y') }}
           </div>
         </div>
       </div>
